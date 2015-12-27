bcd5fef141f9ba6876c71f9054df6d89c9893009
Dominic Plouffe <dominic@dplouffe.ca>
2015-12-27 18:56:08 -0500

    different touch

--- game.js
+++ game.js
@@ -295,7 +295,7 @@ function game() {
     this.zIndex = 10;
     this.pipes = [];
 
-    $('.click_box').on('tap',function (e){
+    $('.click_box').on('touch',function (e){
         var is_left = $(this).hasClass('left');
         if (!g.game_over){
             if (is_left) {
