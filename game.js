3f137f56dc9e6557eb87d761befecb50c4d98ea0
Dominic Plouffe <dominic@dplouffe.ca>
2015-12-28 08:27:51 -0500

    removed sound, added back the statusbar disabler

--- game.js
+++ game.js
@@ -162,7 +162,7 @@ function game() {
 
                 this.coins += 1;
                 this.set_coins(this.coins);
-                this.sounds.coins.play();
+                // this.sounds.coins.play();
             } else if (space.shield) {
                 space.shield = false;
                 space._el.empty();
@@ -289,8 +289,8 @@ function game() {
 
     //SOUND
     this.sounds = {
-        'coins': new Media('sounds/coin.wav'),
-        'shields': new Media('sounds/shield.wav'),
+        'coins': new Audio('sounds/coin.wav'),
+        'shields': new Audio('sounds/shield.wav'),
     };
 
     var g = this;
