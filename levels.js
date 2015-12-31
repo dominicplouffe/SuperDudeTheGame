594cb1dc57a374c4932913f1fb3929b625d8ad25
Dominic Plouffe <dominic@dplouffe.ca>
2015-12-31 14:09:35 -0500

    improved level 1 usability

--- levels.js
+++ levels.js
@@ -4,7 +4,7 @@ function set_level(level, game) {
     if (level === 1) {
         game.default_move_rate = 2;
         // game.pipe_move_rate = 2;
-        game.vertical_space = 70;
+        game.vertical_space = 120;
     } else if (level === 2) {
         game.default_move_rate = 3;
         // game.pipe_move_rate = 3;
